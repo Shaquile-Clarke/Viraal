36352343f840913c2883bdc4fe67c9ae3d3b0c88
Shaquile Clarke <Shaquile.v.clarke@gmail.com>
2022-06-23 14:31:45 -0400

    changed to fixed header

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -4,7 +4,7 @@ import { SearchIcon, UserIcon } from "@heroicons/react/outline";
 
 export default function MainHeader() {
   return (
-    <header className="flex p-1 text-white bg-[#212121] drop-shadow-lg z-10 justify-center items-center sticky top-0">
+    <header className="flex p-1 text-white bg-[#212121] drop-shadow-lg z-10 justify-center items-center top-0 fixed w-full ">
       <Link href={`/`}>
         <a className="font-semibold text-5xl px-4 bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-yellow-500 overflow-hidden">
           V
