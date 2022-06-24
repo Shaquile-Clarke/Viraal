c66c0ab2973dc5c17d78befc889e0fb09aca57a8
Shaquile Clarke <Shaquile.v.clarke@gmail.com>
2022-06-24 12:36:09 -0400

    added Path component for links

--- components/streamer/live-channel.js
+++ components/streamer/live-channel.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Path from "../ui/path";
 import Tag from "../streamer/tag";
 
 export default function LiveChannel({
@@ -12,35 +13,43 @@ export default function LiveChannel({
 }) {
   return (
     <div>
-      <div className="flex w-full overflow-hidden">
-        <Image
-          src={image}
-          alt={`${streamerName} stream preview`}
-          height={300}
-          width={600}
-        />
-        <div className="absolute px-2 font-semibold bg-red-600 rounded-md ml-2 mt-1 text-sm">
-          LIVE
-        </div>
-        <div className="absolute px-2 font-semibold bg-gray-700/40 rounded-md ml-2 mt-8  text-sm">
-          {viewCount > 1000
-            ? (viewCount / 1000).toFixed(1) + "K"
-            : `${viewCount}`}{" "}
-          viewers
-        </div>
-      </div>
-      <div className="flex h-auto w-full mt-2 jus">
-        <div className="h-10 w-10 overflow-hidden">
+      <Path href={`/${streamerName}`}>
+        <div className="flex w-full overflow-hidden">
           <Image
-            src={profileImage}
-            alt={`${streamerName} Logo`}
-            width={80}
-            height={80}
+            src={image}
+            alt={`${streamerName} stream preview`}
+            height={300}
+            width={600}
           />
+          <div className="absolute px-2 font-semibold bg-red-600 rounded-md ml-2 mt-1 text-sm">
+            LIVE
+          </div>
+          <div className="absolute px-2 font-semibold bg-gray-700/40 rounded-md ml-2 mt-8  text-sm">
+            {viewCount > 1000
+              ? (viewCount / 1000).toFixed(1) + "K"
+              : `${viewCount}`}{" "}
+            viewers
+          </div>
         </div>
+      </Path>
+      <div className="flex w-full mt-2 jus">
+        <Path href={`/${streamerName}`}>
+          <div className="h-10 w-10 overflow-hidden">
+            <Image
+              src={profileImage}
+              alt={`${streamerName} Logo`}
+              width={80}
+              height={80}
+            />
+          </div>
+        </Path>
         <div className="flex flex-col text-left ml-3 ">
-          <p className="font-semibold">{title}</p>
-          <span>{streamerName}</span>
+          <Path href={`/${streamerName}`}>
+            <p className="font-semibold">{title}</p>
+          </Path>
+          <Path href={`/${streamerName}`}>
+            <span>{streamerName}</span>
+          </Path>
           <span>{game}</span>
           <div className="flex flex-wrap">
             {categories.map((tags) => (
